17baa3afb194468481f8545a4fb0e92d1000417b
jviriato <josevviriato@gmail.com>
2019-06-30 23:23:08 -0300

    Add mandelbrot

---
+++ trabalhos/t9/mandelbrot/mandelbrot.cu
@@ -0,0 +1,61 @@
+#include <complex>
+#include <iostream>
+#include <sys/time.h>
+#include <thrust/complex.h>
+
+using namespace std;
+
+__global__ void make_fractal(char *mat, int max_n, int max_row, int max_column){
+
+   int r = threadIdx.x;
+
+   using namespace std;
+
+		for(int c = 0; c < max_column; ++c){
+			thrust::complex<float> z;
+			int n = 0;
+         while(z.real() < 2 && ++n < max_n)
+				z = pow(z, 2) + decltype(z)(
+					(float)c * 2 / max_column - 1.5,
+					(float)r * 2 / max_row - 1
+				);
+			mat[r*max_column + c]=(n == max_n ? '#' : '.');
+		}
+	//}
+
+}
+
+int main(int argc, char *argv[]){
+	int max_row, max_column, max_n, print;
+
+	if (argc != 5){
+          std::cout << "Faltam argumentos, devem ser no formato: ./executavel max_row max_column max_n" << std::endl;
+          exit(-1);
+	}
+	max_row = atoi(argv[1]);
+	max_column = atoi(argv[2]);
+	max_n = atoi(argv[3]);
+	print = atoi(argv[4]);
+	char *mat;
+	cudaMallocManaged(&mat, max_row*max_column*sizeof(unsigned char));
+   	timeval start, end;
+   	gettimeofday(&start, NULL);
+
+   	make_fractal<<<1, max_row>>>(mat, max_n, max_row, max_column);
+
+	//Barreira para esperar todas as threads terminarem
+   	cudaDeviceSynchronize();
+
+
+	gettimeofday(&end, NULL);
+   	double runtime = end.tv_sec + end.tv_usec / 1000000.0 - start.tv_sec - start.tv_usec / 1000000.0;
+   	std::cout << "compute time: " << runtime << " s\n";
+
+	if(print){
+          for(int r = 0; r < max_row; ++r){
+            for(int c = 0; c < max_column; ++c)
+              std::cout << mat[r*max_column + c];
+	        out << '\n';
+	      }
+	   }
+}
